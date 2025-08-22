58a24bf319d0643e142cfe8f0ac575a6b29ad35d
Weins <wsjo3jo3@gmail.com>
2025-08-22 16:17:28 +0800

    feat: Add trialsController and related DTOs for GET trial templates api

---
+++ GoBetGoal_BackEnd/Models/DTOs/StageInfoDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoBetGoal_BackEnd.Models.DTOs
+{
+    public class StageInfoDto
+    {
+        public int StageIndex { get; set; }
+
+        public string StageDescription { get; set; }
+
+        public string StageSampleImagePath { get; set; }
+
+    }
+}
---
+++ GoBetGoal_BackEnd/Models/DTOs/TrialTemplateListDto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GoBetGoal_BackEnd.Models.DTOs
+{
+    public class TrialTemplateListDto
+    {
+        public int Id { get; set; }
+
+        public string TrialTitle { get; set; }
+
+        public string TrialDescription { get; set; }
+
+        public int TrialFrequency { get; set; }
+
+        public string TrialCategory { get; set; }
+
+        public string TrialSuitFor { get; set; }
+
+        public string TrialNoSuitFor { get; set; }
+
+        public string TrialRule { get; set; }
+
+        public string TrialCaution { get; set; }
+
+        public string TrialEffect { get; set; }
+
+        public int StageCount { get; set; }
+
+
+        public int MaxUser { get; set; }
+
+        public bool IsAi { get; set; }
+
+        public int TrialTemplatePrice { get; set; }
+
+        //public ProductType ProductType { get; set; }
+
+        public string CardImagePath { get; set; }
+
+        public string CardColor { get; set; }
+
+        public List<StageInfoDto> Stages { get; set; }
+    }
+}
