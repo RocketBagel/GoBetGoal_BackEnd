986256b758f13e6751922e528ceb5bab3f1a8c66
kc34522 <kelly556320@gmail.com>
2025-08-27 01:04:37 +0800

    feat: add purchase avatar api and add purchase response dto

---
+++ GoBetGoal_BackEnd/Models/DTOs/PurchaseSuccessDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoBetGoal_BackEnd.Models.DTOs
+{
+    /// <summary>
+    /// 通用的成功回應
+    /// </summary>
+    public class PurchaseSuccessDto
+    {
+        public string Message { get; set; }
+        public int RemainingBagelCount { get; set; }
+    }
+}
