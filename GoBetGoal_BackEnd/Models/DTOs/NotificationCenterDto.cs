910a3612a0520a125755cdfdea092b0b9b41f5c1
kc34522 <kelly556320@gmail.com>
2025-08-31 22:31:09 +0800

    feat: add get all nitifications api

---
+++ GoBetGoal_BackEnd/Models/DTOs/NotificationCenterDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoBetGoal_BackEnd.Models.DTOs
+{
+    public class NotificationCenterDto
+    {
+        /// <summary>
+        /// 公告類型的通知
+        /// </summary>
+        public List<NotificationDto> Announcements { get; set; }
+
+        /// <summary>
+        /// 未讀的個人通知
+        /// </summary>
+        public List<NotificationDto> Unread { get; set; }
+
+        /// <summary>
+        /// 已讀的個人通知
+        /// </summary>
+        public List<NotificationDto> Read { get; set; }
+    }
+}
