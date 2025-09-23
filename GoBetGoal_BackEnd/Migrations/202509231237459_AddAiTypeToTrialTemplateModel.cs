b3c93e53f9a072427803c7976705e514099e05a8
kc34522 <kelly556320@gmail.com>
2025-09-23 21:16:36 +0800

    fix: add ai type to Trial Template model

---
+++ GoBetGoal_BackEnd/Migrations/202509231237459_AddAiTypeToTrialTemplateModel.cs
@@ -0,0 +1,18 @@
+﻿namespace GoBetGoal_BackEnd.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAiTypeToTrialTemplateModel : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.TrialTemplates", "AiType", c => c.String(maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.TrialTemplates", "AiType");
+        }
+    }
+}
