a32b2b98a34729154db3fdd9eb629fbddb290e11
kc34522 <kelly556320@gmail.com>
2025-09-25 15:20:13 +0800

    fix: delete ai related column in stage model

---
+++ GoBetGoal_BackEnd/Migrations/202509250713206_DeleteAiRelatedColumnInStageModel.cs
@@ -0,0 +1,20 @@
+﻿namespace GoBetGoal_BackEnd.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeleteAiRelatedColumnInStageModel : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Stages", "VerificationMode");
+            DropColumn("dbo.Stages", "ChallengeType");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Stages", "ChallengeType", c => c.String(maxLength: 50));
+            AddColumn("dbo.Stages", "VerificationMode", c => c.String(maxLength: 50));
+        }
+    }
+}
